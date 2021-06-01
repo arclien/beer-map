9aa698eff4f54cc0398c9129c99f0f37a0fe8e03
Aaron <aaron@grepp.co>
2021-06-01 13:35:26 +0900

    project setting

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  module: {
+    rules: [
+      // ... other rules omitted
+
+      // this will apply to both plain `.scss` files
+      // AND `<style lang="scss">` blocks in `.vue` files
+      {
+        test: /\.scss$/,
+        use: ['vue-style-loader', 'css-loader', 'sass-loader'],
+      },
+
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              // enable CSS Modules
+              modules: true,
+              // customize generated class names
+              localIdentName: '[local]_[hash:base64:8]',
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
