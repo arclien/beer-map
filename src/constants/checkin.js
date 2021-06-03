8e0a53ea1ef594b019b124c7eeb9a24c9b26df1e
Aaron <aaron@grepp.co>
2021-06-03 14:45:37 +0900

    modal 로직 추가

--- src/constants/checkin.js
+++ src/constants/checkin.js
@@ -1,1 +1,1 @@
-export const CHECK_IN_HISTORY = 'beer_map_check_in_history';
+export const CHECK_IN_HISTORY = '@beer_map_check_in_history';
--- src/views/Collection/CollectionModalView.vue
+++ src/views/Collection/CollectionModalView.vue
@@ -6,17 +6,29 @@
       :close-modal-callback="closeModalCallback"
       :on-header-button-click="() => {}">
       <template #body>
-        <div>
-          <ul>
-            <li v-for="item in checkInList" :key="item.id" :class="$style.item" @click="$parent.$emit('show-place-info-window', item.id)">
-              <div :class="$style.name">
-                {{ getMapDataById(item.id).name }}
-              </div>
-              <div :class="$style.date">
-                {{ item.date.length }}회 방문. {{ item.date.join(", ") }}
-              </div>
-            </li>
-          </ul>
+        <div :class="$style.container">
+          <template v-if="checkInList.length">
+            <div :class="$style.summarize">
+              {{ summarize }}
+              {{ totalCount }}
+            </div>
+            <ul>
+              <li v-for="item in checkInList" :key="item.id" :class="$style.item" @click="$parent.$emit('show-place-info-window', item.id)">
+                <div :class="$style.name">
+                  {{ getMapDataById(item.id).name }}
+                </div>
+                <div :class="$style.date">
+                  {{ item.date.length }}회 방문. {{ item.date.join(", ") }}
+                </div>
+              </li>
+            </ul>
+          </template>
+          <template v-else>
+            <div :class="$style.empty">
+              <p>체크인 히스토리가 없습니다! 분발해주세요!</p>
+              <img src="@/assets/images/empty.png" alt="empty list">
+            </div>
+          </template>
         </div>
       </template>
     </MobileFullHeightModal>
@@ -26,6 +38,7 @@
 <script>
 
 import MobileFullHeightModal from '@/components/Modal/MobileFullHeightModal';
+import MapData from '@/constants/mapData';
 import MobileFullHeightModalMixin from '@/mixins/MobileFullHeightModal';
 import { getCheckedInData } from '@/utils/CheckedIn.utils';
 import { getMapDataById } from '@/utils/MapData.utils';
@@ -47,6 +60,17 @@ export default {
       modalId: 'collection',
     };
   },
+  computed: {
+    summarize() {
+      return `${this.checkInList.length} / ${MapData.length} Checked In!  `;
+    },
+    totalCount() {
+      const total = this.checkInList.reduce((acc, cur) => {
+        return acc + cur.date.length;
+      }, 0);
+      return ` Total ${total}`;
+    },
+  },
   created() {
     this.checkInList = getCheckedInData().sort((a, b) => b.date.length - a.date.length);
   },
@@ -61,14 +85,20 @@ export default {
       return getMapDataById(id);
     },
   },
-
 };
 </script>
 
 <style lang="scss" module>
+.container{
+  height:100%;
+}
 .item{
   padding: 10px;
   border-bottom:1px solid #ddd;
+
+  &:nth-child(2n){
+    background-color: #fafafa;
+  }
 }
 .name{
   font-size: 16px;
@@ -76,5 +106,25 @@ export default {
 .date{
   font-size: 12px;
   color: #aaa;
+}
+.summarize{
+  background-color:#e2e2e2;
+  font-size: 18px;
+  padding:5px;
+  text-align: center;
+}
+.empty{
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  height: 100%;
+  p{
+    font-size:16px;
+  }
+  img{
+    width:100%;
+    height:auto;
+  }
 }
 </style>
